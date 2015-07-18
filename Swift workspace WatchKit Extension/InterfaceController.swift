13a930b561a34db1c5c474ae8d73d8e9fe9f1193
Patryk Raba <kontakt@tested.pl>
2015-07-18 11:37:17 +0200

    First commit

---
+++ Swift workspace WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Swift workspace WatchKit Extension
+//
+//  Created by Patryk Raba on 18.07.2015.
+//  Copyright (c) 2015 Patryk Raba. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
